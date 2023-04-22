36c477ceeb7f12cccc46c977b1b7079d09d3b220
jHhoseph988 <131305638+jHhoseph988@users.noreply.github.com>
2023-04-22 14:31:27 +0200

    Cambie el 4 por el 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,20 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 5000,
+
+    toHaveScreenshot: {
+      // An acceptable amount of pixels that could be different, unset by default.
+      maxDiffPixels: 10,
+    },
+
+    toMatchSnapshot: {
+      // An acceptable ratio of pixels that are different to the total amount of pixels, between 0 and 1.
+      maxDiffPixelRatio: 10,
+    },
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
--- tests/registroUsuario.spec.ts
+++ tests/registroUsuario.spec.ts
@@ -1,6 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('registro exisitoso', async ({ page }) => {
+    await page.goto('https://buggy.justtestit.org/');
+    await page.getByRole('link', { name: 'Register' }).click();
+    await page.getByLabel('Login').click();
+    await page.getByLabel('Login').fill('usernamejosep4');
+    await page.getByLabel('First Name').click();
+    await page.getByLabel('First Name').fill('josep');
+    await page.getByLabel('Last Name').click();
+    await page.getByLabel('Last Name').fill('pepito');
+    await page.getByLabel('Password', { exact: true }).click();
+    await page.getByLabel('Password', { exact: true }).fill('Qwe12345@');
+    await page.getByLabel('Confirm Password').click();
+    await page.getByLabel('Confirm Password').fill('Qwe12345@');
+    await page.getByRole('button', { name: 'Register' }).click();
+    await page.getByText('Registration is successful').click();
+});
+test('validar que no se puede registrar con un usuario existente', async ({ page }) => {
     await page.goto('https://buggy.justtestit.org/');
     await page.getByRole('link', { name: 'Register' }).click();
     await page.getByLabel('Login').click();
@@ -14,5 +30,5 @@ test('test', async ({ page }) => {
     await page.getByLabel('Confirm Password').click();
     await page.getByLabel('Confirm Password').fill('Qwe12345@');
     await page.getByRole('button', { name: 'Register' }).click();
-    await page.getByText('Registration is successful').click();
+    await page.getByText('UsernameExistsException: User already exists').click();
 });
